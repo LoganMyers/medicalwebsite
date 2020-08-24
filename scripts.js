31b71afeea1f6bd283946d09d117882cfa369849
LoganMyers <68205196+LoganMyers@users.noreply.github.com>
2020-08-24 12:52:23 -0400

    Uploaded whole content
    Replaced all files with their updated versions (.css, .js, .html, and images folder).

---
+++ scripts.js
@@ -0,0 +1,13 @@
+
+function switchImage1()  {
+    document.getElementById('firstImage').src = "images/group-of-doctors.jpg";
+}
+function switchImage2() {
+    document.getElementById('firstImage').src = "images/pharmacist.jpg"; 
+}
+function switchImage3() {
+    document.getElementById('firstImage').src = "images/medical-insurance.jpg";
+}
+function switchImage4() {
+    document.getElementById('firstImage').src = "images/health-chart.jpg";
+}
